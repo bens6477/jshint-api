a878f6fa40fade9f6c1d9f0ac1ea95321cc85915
Ben Smith <bens6477@gmail.com>
2022-01-25 14:05:41 +0000

    Make get request to API URL with API key and display data in modal

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+const API_KEY = "nzWWZjKM4CPLNHaaKQoFd15R-yI";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+// Function to make a request to the API URL with the API key.
+// Function to pass the data to a display function
+ 
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        displayStatus(data);
+    } else {
+        throw new Error(data.error);
+    }
+}
+
+function displayStatus(data) {
+    let heading = "API Key Status";
+    let results = `<div>Your key is valid until</div>`;
+    results += `<div class="key-status">${data.expiry}</div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
+}
